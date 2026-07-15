46681af182dc6b1f5722846843ca9e86a5a54572
Максим <maxwell884884@gmail.com>
2026-07-15 14:03:00 +0300

    Route profile through authenticated CRM app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { CrmAppPage } from './pages/CrmAppPage'
+
+const rootPage = window.location.pathname === '/profile'
+  ? <CrmAppPage />
+  : <App />
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {rootPage}
   </StrictMode>,
 )
