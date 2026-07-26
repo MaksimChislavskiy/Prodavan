28e19894fac0608a5cf9d56b3615279c35732eef
Максим <maxwell884884@gmail.com>
2026-07-26 13:59:50 +0300

    Load password reset mobile fixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './PasswordResetModal.mobile.css'
 import { AccountDeletedNotice } from './components/AccountDeletedNotice'
 import { CrmAppPage } from './pages/CrmAppPage'
 
