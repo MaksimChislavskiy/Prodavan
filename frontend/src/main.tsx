89411faa60380e17e9a89953ab1a7ead9d263977
Максим <maxwell884884@gmail.com>
2026-09-04 12:25:25 +0300

    Remove obsolete contacts DOM contract controller

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,6 @@ import { AccountDeletedNotice } from './components/AccountDeletedNotice'
 import { CrmToastController } from './components/crm/CrmToastController'
 import { CrmAppPage } from './pages/CrmAppPage'
 import { installAiSettingsFeedbackController } from './shared/aiSettingsFeedbackController'
-import { installContactsContractController } from './shared/contactsContractController'
 import {
   CRM_REALTIME_EVENT,
   CRM_REALTIME_RECONNECTED_EVENT,
@@ -24,7 +23,6 @@ installCrmRealtime()
 installTasksTouchDnd()
 installDealsTouchDnd()
 installDealsContractController()
-installContactsContractController()
 
 const AI_SETTINGS_SPEC_PATH = '/settings/ai'
 const AI_SETTINGS_APP_PATH = '/app/settings/ai'
